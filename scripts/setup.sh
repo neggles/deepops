e235bacee79f9a3b3395ba93c88d529ddac49b77
neggles <aholmes@omnom.net>
2026-03-06 02:19:45 +0000

    switch to `uv`

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,35 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# DeepOps setup/bootstrap script
-#   This script installs required dependencies on a system so it can run Ansible
-#   and initializes the DeepOps directory
-#
-# Can be run standalone with: curl -sL bit.ly/nvdeepops | bash
-#                         or: curl -sL bit.ly/nvdeepops | bash -s -- 19.07
-
 # Determine current directory and root directory
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 ROOT_DIR=$(realpath "${SCRIPT_DIR}/../")
 
 # Configuration
-ANSIBLE_VERSION="${ANSIBLE_VERSION:-9.13.0}"        # Ansible version to install
-ANSIBLE_TOO_NEW="${ANSIBLE_TOO_NEW:-10.0.1}"        # Ansible version too new
-ANSIBLE_LINT_VERSION="${ANSIBLE_LINT_VERSION:-5.4.0}"
-CONFIG_DIR="${CONFIG_DIR:-${ROOT_DIR}/config}"      # Default configuration directory location
-DEEPOPS_TAG="${1:-master}"                          # DeepOps branch to set up
-JINJA2_VERSION="${JINJA2_VERSION:-3.1.5}"           # Jinja2 required version
-JMESPATH_VERSION="${JMESPATH_VERSION:-0.10.0}"      # jmespath pegged version, actual version probably not that crucial
-MARKUPSAFE_VERSION="${MARKUPSAFE_VERSION:-3.0.2}"   # MarkupSafe version
-PIP="${PIP:-pip3}"                                  # Pip binary to use
-PYTHON_BIN="${PYTHON_BIN:-/usr/bin/python3.10}"     # Python3 path
-VENV_DIR="${VENV_DIR:-$ROOT_DIR/.venv}"             # Path to python virtual environment to create
+CONFIG_DIR="${CONFIG_DIR:-$ROOT_DIR/config}"  # Path to configuration directory
+VIRTUAL_ENV="${VIRTUAL_ENV:-$ROOT_DIR/.venv}" # Path to python virtual environment to create
 
-###
+if ! command -v uv &>/dev/null; then
+	echo "uv is required to manage this project's virtual environment. Please install uv to use this project. "
+	echo "Installation instructions: https://docs.astral.sh/uv/getting-started/installation/"
+	exit 1
+fi
 
 # Set distro-specific variables
 . /etc/os-release
-DEPS_DEB=(git python3.10-venv sshpass wget curl)
+DEPS_DEB=(git python3.12-venv python3.12-pip sshpass wget curl)
 
 # Disable interactive prompts from Apt
 export DEBIAN_FRONTEND=noninteractive
@@ -37,104 +25,71 @@ export DEBIAN_PRIORITY=critical
 
 # Exit if run as root
 if [[ $(id -u) -eq 0 ]]; then
-    echo "Please run as a regular user"
-    exit
+	echo "This script/repository does not support running as root, please run as a normal user"
+	exit 1
 fi
 
 # Install software dependencies
 case "$ID" in
-    ubuntu*)
-        sudo apt-get -q update
-        sudo apt-get -yq install ${DEPS_DEB[@]}
-        ;;
-    *)
-        echo "Unsupported Operating System $ID_LIKE"
-        echo "Please install ${DEPS_RPM[@]} manually"
-        ;;
+ubuntu*)
+	sudo apt-get -q update
+	sudo apt-get -yq install ${DEPS_DEB[@]}
+	;;
+*)
+	echo "Unsupported Operating System $ID_LIKE"
+	echo "Please install ${DEPS_DEB[@]} manually"
+	;;
 esac
 
-if ! ${PYTHON_BIN} -m venv --help &>/dev/null; then
-    echo "ERROR: Unable to create Python virtual environment, 'venv' module not found"
-    exit 1
+# read .python-version file
+py_version=$(<"${ROOT_DIR}/.python-version)")
+
+# resolve venv path and create it if it doesn't exist
+if [[ ! -d "${VIRTUAL_ENV}" ]]; then
+	echo "Creating project virtual environment with uv"
+	uv venv --seed --python "${py_version}" "${VIRTUAL_ENV}"
 fi
 
-# Create virtual environment and install python dependencies
-if [[ ! -d ${VENV_DIR} || ! -f ${VENV_DIR}/bin/activate ]]; then
-    echo "Creating Python virtual environment in ${VENV_DIR}"
-    deactivate nondestructive &>/dev/null || true # Deactivate any existing virtualenv
-    ${PYTHON_BIN} -m venv $(realpath "${VENV_DIR}")
-    source "${VENV_DIR}/bin/activate"
-    python -m pip install -q --upgrade pip setuptools wheel
-fi
-source "${VENV_DIR}/bin/activate"
+# activate the virtual environment
+source "${VIRTUAL_ENV}/bin/activate"
+# configure uv
+export UV_PROJECT="${ROOT_DIR}"
+export UV_LINK_MODE="copy" # use copy mode so ansible doesn't get angy
 
-# Check for any installed ansible pip package
-if pip show ansible 2>&1 >/dev/null; then
-    current_version=$(pip show ansible | grep Version | awk '{print $2}')
-    echo "Current version of Ansible is ${current_version}"
-    if python -c "from distutils.version import LooseVersion; print(LooseVersion('$current_version') >= LooseVersion('$ANSIBLE_TOO_NEW'))" | grep True 2>&1 >/dev/null; then
-        echo "Ansible version ${current_version} too new for DeepOps"
-        echo "Please uninstall any ansible, ansible-base, and ansible-core packages and re-run this script"
-        exit 1
-    elif python -c "from distutils.version import LooseVersion; print(LooseVersion('$current_version') < LooseVersion('$ANSIBLE_VERSION'))" | grep True 2>&1 >/dev/null; then
-        echo "Ansible will be upgraded from ${current_version} to ${ANSIBLE_VERSION}"
-    fi
-fi
+# sync environment with pyproject.toml
+echo "Ensuring venv packages are up to date"
+uv sync --inexact --no-lock
 
-echo "Installing Python dependencies..."
-cat <<EOF >${VENV_DIR}/requirements.txt
-ansible==${ANSIBLE_VERSION}
-ansible-lint==${ANSIBLE_LINT_VERSION}
-Jinja2==${JINJA2_VERSION}
-netaddr
-ruamel.yaml
-PyMySQL
-paramiko
-requests==2.31.0
-jmespath==${JMESPATH_VERSION}
-MarkupSafe==${MARKUPSAFE_VERSION}
-selinux
-EOF
-python -m pip install -q --require-virtualenv --upgrade -r ${VENV_DIR}/requirements.txt
-
-# Copy default configuration
+# If the config directory doesn't exist, copy the example config there.
 if [[ ! -d ${CONFIG_DIR} ]]; then
-    cp -rfp "${ROOT_DIR}/config.example" "${CONFIG_DIR}"
-    echo "Copied default configuration to ${CONFIG_DIR}"
-else
-    echo "Configuration directory '${CONFIG_DIR}' exists, not overwriting"
+	echo "Configuration directory '${CONFIG_DIR}' does not exist, copying default configuration"
+	cp -a "${ROOT_DIR}/config.example" "${CONFIG_DIR}"
 fi
 
 # Install Ansible Galaxy roles
 if command -v ansible-galaxy &>/dev/null; then
-    echo "Updating Ansible Galaxy roles..."
-    initial_dir="$(pwd)"
-    roles_path="${ROOT_DIR}/roles/galaxy"
-    collections_path="${ROOT_DIR}/collections"
+	echo "Updating Ansible Galaxy roles..."
+	roles_path="${ROOT_DIR}/roles/galaxy"
+	collections_path="${ROOT_DIR}/collections"
 
-    cd "${ROOT_DIR}"
-    ansible-galaxy collection install -p "${collections_path}" --force -r "roles/requirements.yml" >/dev/null
-    ansible-galaxy role install -p "${roles_path}" --force -r "roles/requirements.yml" >/dev/null
+	pushd "${ROOT_DIR}" >/dev/null
+	ansible-galaxy collection install -p "${collections_path}" --force -r "roles/requirements.yml" >/dev/null
+	ansible-galaxy role install -p "${roles_path}" --force -r "roles/requirements.yml" >/dev/null
 
-    # Install any user-defined config requirements
-    if [[ -d ${CONFIG_DIR} && -f "${CONFIG_DIR}/requirements.yml" ]]; then
-        cd "${CONFIG_DIR}"
-        ansible-galaxy collection install -p "${collections_path}" --force -i -r "requirements.yml" >/dev/null
-        ansible-galaxy role install -p "${roles_path}" --force -i -r "requirements.yml" >/dev/null
-    fi
-    cd "${initial_dir}"
+	# Install any user-defined config requirements
+	if [[ -d "${CONFIG_DIR}" && -f "${CONFIG_DIR}/requirements.yml" ]]; then
+		cd "${CONFIG_DIR}"
+		ansible-galaxy collection install -p "${collections_path}" --force -i -r "requirements.yml" >/dev/null
+		ansible-galaxy role install -p "${roles_path}" --force -i -r "requirements.yml" >/dev/null
+	fi
+	popd >/dev/null
 else
-    echo "ERROR: Unable to install Ansible Galaxy roles, 'ansible-galaxy' command not found"
+	echo "ERROR: Unable to install Ansible Galaxy roles, 'ansible-galaxy' command not found"
 fi
 
-# Update submodules
-if command -v git &>/dev/null; then
-    git submodule update --init --recursive
-else
-    echo "ERROR: Unable to update Git submodules, 'git' command not found"
-fi
+
 
 echo
 echo "*** Setup complete ***"
-echo "To use Ansible, run: source ${VENV_DIR}/bin/activate"
+echo "To use Ansible, run: source ${VIRTUAL_ENV}/bin/activate"
 echo
